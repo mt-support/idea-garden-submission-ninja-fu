e02d0a83e1e59c210541ab0e6144ccf242325013
geoffgraham <geoff@geoffgraham.me>
2018-08-06 12:27:05 -0700

    Updates public list item markup and class names for card component.
    Signed-off-by: geoffgraham <geoff@geoffgraham.me>

--- php/views/public-list.php
+++ php/views/public-list.php
@@ -8,16 +8,23 @@ namespace Modern_Tribe\Idea_Garden\Ninja_Fu;
 ?>
 
 <section class="idea-garden-public-list">
-	<ol>
+	<ol class="ig-list">
 		<?php foreach ( $ideas as $idea ): ?>
-			<li>
-				<h1> <?php echo esc_html( $idea->idea ); ?> </h1>
-				<p class="elevator"> <?php echo esc_html( $idea->elevator_pitch ); ?> </p>
-				<p class="product">
-					<?php foreach( (array) $idea->product as $selected_product): ?>
-						<span> <?php echo esc_html( $selected_product ); ?> </span>
-					<?php endforeach; ?>
-				</p>
+			<li class="ig-idea-list__card">
+				<div class="ig-idea-list__header">
+					<?php echo get_avatar(); ?>
+					<h1> <?php echo esc_html( $idea->idea ); ?> </h1>
+					<div class="ig-idea-list__meta">
+						<span class="ig-idea-list__status">Open</span>
+						<?php echo get_the_date( $d = 'M j, Y' ); ?>
+						<?php foreach( (array) $idea->product as $selected_product): ?>
+							<span> <?php echo esc_html( $selected_product ); ?> </span>
+						<?php endforeach; ?>
+					</div>
+				</div>
+				<div class="ig-idea-list__content">
+					<p class="elevator"> <?php echo esc_html( $idea->elevator_pitch ); ?> </p>
+				</div>
 
                 <?php do_action( 'idea_garden.ninja_fu.submission_voting_form', $idea ); ?>
 			</li>
