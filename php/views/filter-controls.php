1818bd6bed7b905b3cfaa2dee9c608cf6480ddc2
barryhughes <barry@codingkills.me>
2018-08-31 16:22:51 -0700

    Populate the product and status selectors dynamically

--- php/views/filter-controls.php
+++ php/views/filter-controls.php
@@ -20,34 +20,31 @@ $product_options = $product_field
 
   <div class="ig-accordion__content">
     <nav class="ig-filters">
-        <select class="ig-filters__filter">
+        <select class="ig-filters__filter" name="ig-product">
             <option value="" selected="selected">All Products</option>
-            <option value="The Events Calendar"></option>
-            <option value="">Events Calendar PRO</option>
-            <option value="">Event Tickets</option>
-            <option value="">Event Tickets Plus</option>
-            <option value="">Community Events</option>
-            <option value="">Community Tickets</option>
-            <option value="">Event Aggregator</option>
-            <option value="">Filter Bar</option>
-            <option value="">Eventbrite Tickets</option>
+            <?php foreach ( $product_options as $product_name => $product_value ): ?>
+                <option value="<?php echo esc_attr( $product_value ); ?>">
+                    <?php echo esc_html( $product_name ); ?>
+                </option>
+            <?php endforeach; ?>
         </select>
 
-        <select class="ig-filters__filter">
+        <select class="ig-filters__filter" name="ig-status">
             <option value="" selected="selected">All Statuses</option>
-            <option value="">Planned</option>
-            <option value="">Started</option>
-            <option value="">In Development</option>
-            <option value="">In Testing</option>
-            <option value="">Completed</option>
-            <option value="">Backlog</option>
+            <?php foreach ( Idea_Statuses::STATES as $idea_slug => $idea_properties ): ?>
+                <?php if ( $idea_properties['public'] ): ?>
+                    <option value="<?php echo esc_attr( $idea_slug ); ?>">
+                        <?php echo esc_html( $idea_properties['label'] ); ?>
+                    </option>
+                <?php endif; ?>
+            <?php endforeach; ?>
         </select>
 
-        <select class="ig-filters__filter">
+        <select class="ig-filters__filter" name="ig-order-sequence">
             <option value="" disabled="disabled" selected="selected">Sort By</option>
-            <option value="">Date</option>
-            <option value="">Idea Status</option>
-            <option value="">Votes</option>
+            <option value="date">Date</option>
+            <option value="status">Idea Status</option>
+            <option value="count">Votes</option>
         </select>
     </nav>
   </div>
