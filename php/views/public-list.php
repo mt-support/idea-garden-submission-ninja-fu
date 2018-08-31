d0e06231cf385f4ed69829ec3d172916f311daca
barryhughes <barry@codingkills.me>
2018-08-31 13:22:28 -0700

    Place public list filters in a template file of their own

--- php/views/public-list.php
+++ php/views/public-list.php
@@ -8,35 +8,7 @@ namespace Modern_Tribe\Idea_Garden;
 ?>
 
 <nav class="ig-filters">
-	<select class="ig-filters__filter">
-		<option value="" selected="selected">All Products</option>
-		<option value="The Events Calendar"></option>
-		<option value="">Events Calendar PRO</option>
-		<option value="">Event Tickets</option>
-		<option value="">Event Tickets Plus</option>
-		<option value="">Community Events</option>
-		<option value="">Community Tickets</option>
-		<option value="">Event Aggregator</option>
-		<option value="">Filter Bar</option>
-		<option value="">Eventbrite Tickets</option>
-	</select>
-
-	<select class="ig-filters__filter">
-		<option value="" selected="selected">All Statuses</option>
-		<option value="">Planned</option>
-		<option value="">Started</option>
-		<option value="">In Development</option>
-		<option value="">In Testing</option>
-		<option value="">Completed</option>
-		<option value="">Backlog</option>
-	</select>
-
-	<select class="ig-filters__filter">
-		<option value="" disabled="disabled" selected="selected">Sort By</option>
-		<option value="">Date</option>
-		<option value="">Idea Status</option>
-		<option value="">Votes</option>
-	</select>
+    <?php echo View::render( 'filter-controls', [ 'helper' => $helper ] ); ?>
 </nav>
 
 <section class="ig-idea-list">
