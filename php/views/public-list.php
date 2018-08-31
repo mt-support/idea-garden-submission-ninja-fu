79664c7cbb9caf008c8241ec5f225c30071e47ec
geoffgraham <geoff@geoffgraham.me>
2018-08-31 15:13:29 -0700

    Styles accordion component.
    Signed-off-by: geoffgraham <geoff@geoffgraham.me>

--- php/views/public-list.php
+++ php/views/public-list.php
@@ -9,10 +9,13 @@ namespace Modern_Tribe\Idea_Garden;
 
 <div class="ig-accordion">
   
-  <input id="toggle1" type="checkbox" name="toggle" />
-  <label for="toggle1">Filters</label>
+  <input id="panel-1" class="ig-accordion__panel" type="checkbox" name="panel" />
+	<label class="ig-accordion__label" for="panel-1">Filters</label>
+	<svg class="ig-accordion__icon" data-icon="chevron" data-containerTransform="translate(0 12)" data-width="null" data-height="null" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 128 128">
+		<path d="M0 .688v22.625l58.344 58.344 5.656 5.656 5.656-5.656 58.344-58.344v-22.625l-64 64-64-64z" transform="translate(0 12)" />
+	</svg>
   
-  <section id="content1">
+  <div class="ig-accordion__content">
 		<nav class="ig-filters">
 			<select class="ig-filters__filter">
 				<option value="" selected="selected">All Products</option>
