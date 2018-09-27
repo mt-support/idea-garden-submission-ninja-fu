d88a473c170b28573640690ceb66dabacaee4c6c
barryhughes <barry@codingkills.me>
2018-09-27 10:04:50 -0700

    Add submission form view

---
+++ php/views/submission-form.php
@@ -0,0 +1,49 @@
+<?php
+namespace Modern_Tribe\Idea_Garden;
+
+use WP_Term;
+
+/**
+ * @var WP_Term[] $categories
+ */
+?>
+
+<section class="ig-idea-submission-form">
+	<form method="post">
+		<label for="ig-idea-submission__idea">
+			<?php _e( 'Idea:', 'idea-garden' ); ?>
+		</label>
+		<input type="text" id="ig-idea-submission__idea" name="idea-title" value="" />
+
+		<?php if ( ! empty( $categories ) ): ?>
+			<label> <?php _e( 'Categories:', 'idea-garden' ); ?> </label>
+		<?php endif; ?>
+
+		<ol>
+			<?php foreach ( $categories as $category ): ?>
+				<li>
+					<input
+						type="checkbox"
+						name="idea-categories[]"
+						id="ig-idea-submission__category__<?php echo esc_attr( sanitize_title( $category->slug ) )?>"
+						value="<?php echo esc_attr( $category->term_id ); ?>"
+					/>
+					<label for="ig-idea-submission__category__<?php echo esc_attr( sanitize_title( $category->slug ) )?>">
+						<?php echo esc_html( $category->name ); ?>
+					</label>
+				</li>
+			<?php endforeach; ?>
+		</ol>
+
+		<label for="ig-idea-submission__description">
+			<?php _e( 'Description:', 'idea-garden' ); ?>
+		</label>
+		<textarea
+			id="ig-idea-submission__description"
+			cols="60"
+			rows="10"
+		></textarea>
+
+		<button><?php esc_html_e( 'Submit', 'idea-garden' ); ?></button>
+	</form>
+</section>
